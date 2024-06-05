bdae5622ee6685603cf06f8b1b80e1ffc97a409b
admin <admin@TeamCity>
2024-06-05 15:34:18 +0100

    TeamCity change in 'Kubernetes Example' project: 'Build' build configuration settings were updated

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -401,6 +401,7 @@ object Build : BuildType({
         feature {
             id = "BUILD_EXT_2"
             type = "KubernetesExecutor"
+            enabled = false
             param("teamcity.kubernetes.executor.container.storageSize", "5Gi")
             param("connectionId", "PROJECT_EXT_37")
         }
