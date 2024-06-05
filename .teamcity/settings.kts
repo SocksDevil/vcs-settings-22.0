b2b54a42952c580d2904c7ac3852321a7f68cbe1
admin <admin@TeamCity>
2024-06-05 16:47:18 +0100

    TeamCity change in 'Kubernetes Example' project: Executor PROJECT_EXT_38 of type KubernetesExecutor was updated

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -275,7 +275,7 @@ project {
         }
         kubernetesExecutor {
             id = "PROJECT_EXT_38"
-            connectionId = "PROJECT_EXT_37"
+            connectionId = "PROJECT_EXT_34"
             profileName = "myKube"
             templateContainer = """
                 apiVersion: v1
